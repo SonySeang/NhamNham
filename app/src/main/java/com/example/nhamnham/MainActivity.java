93a739bf471a7fabdaddbb87749ae8f1fb452cec
#Elzarha <s.seang.sony@gmail.com>
2025-04-26 01:17:06 +0700

    Design-Interface

--- app/src/main/java/com/example/nhamnham/MainActivity.java
+++ app/src/main/java/com/example/nhamnham/MainActivity.java
@@ -1,47 +1,54 @@
 package com.example.nhamnham;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.example.nhamnham.fragments.HomeFragment;
-import com.example.nhamnham.fragments.SearchFragment;
-import com.example.nhamnham.fragments.OrderFragment;
-import com.example.nhamnham.fragments.ProfileFragment;
+
+import com.example.nhamnham.databinding.ActivityMainBinding;
+import com.example.nhamnham.fragment.HomeFragment;
+import com.example.nhamnham.fragment.OrderFragment;
+import com.example.nhamnham.fragment.ProfileFragment;
+import com.example.nhamnham.fragment.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
-        bottomNavigation.setOnItemSelectedListener(item -> {
-            int id = item.getItemId();
-
-            if (id == R.id.nav_home) {
-                loadFragment(new HomeFragment());
-            } else if (id == R.id.nav_search) {
-                loadFragment(new SearchFragment());
-            } else if (id == R.id.nav_order) {
-                loadFragment(new OrderFragment());
-            } else if (id == R.id.nav_profile) {
-                loadFragment(new ProfileFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_home) {
+                LoadFragment(new HomeFragment());
+            } else if (itemId == R.id.nav_search) {
+                LoadFragment(new SearchFragment());
+            } else if (itemId == R.id.nav_order) {
+                LoadFragment(new OrderFragment());
+            } else if (itemId == R.id.nav_user) {
+                LoadFragment(new ProfileFragment());
             } else {
                 return false;
             }
             return true;
         });
 
-        // Load default fragment
-        loadFragment(new HomeFragment());
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        } else {
+            binding.bottomNavigationView.setSelectedItemId(savedInstanceState.getInt("selectedItemId"));
+        }
     }
 
-    private void loadFragment(Fragment fragment) {
+    private void LoadFragment(Fragment fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.fragment_container, fragment) // <-- This should be your container
+                .replace(R.id.container, fragment)
                 .commit();
     }
 }
+
